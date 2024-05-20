4322e63950200ed6d7ce8d688e5a761b2a15af47
Nathan Adams <nathan.zero64@gmail.com>
2024-05-19 21:56:50 -0400

    Removed superfluous print statements, opened domains for CORS

--- inventory.py
+++ inventory.py
@@ -4,7 +4,7 @@ from . import db
 # create blueprint for this module
 bp = Blueprint('inventory', __name__)
 
-domain = "http://localhost:3000"
+domain = "*"
 
 # handle incoming get requests for specific items
 @bp.route("/inventory/<string:item>", methods=["GET"])
@@ -31,7 +31,6 @@ def getInventory(item):
 @bp.route("/inventory", methods=["OPTIONS", "POST"])
 def postInventory():
     if request.method == "OPTIONS":
-        print("returning options response")
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", domain)
         response.headers.add("Access-Control-Allow-Headers", "Content-Type")
@@ -41,12 +40,9 @@ def postInventory():
         inv = db.connect_db()
         cursor = inv.cursor()
         try:
-            if(cursor.execute("SELECT * from inventory WHERE id = '"+(request.json[0].replace(" ","-")).lower()+"'").fetchone()):
-                print("huh?")
-                print("UPDATE inventory SET have ='"+request.json[1]+"', need ='"+request.json[2]+"', checkweekly ='"+request.json[3]+"' WHERE id='"+(request.json[0].replace(" ","-")).lower()+"'")
+            if (cursor.execute("SELECT * from inventory WHERE id = '"+(request.json[0].replace(" ","-")).lower()+"'").fetchone()):
                 cursor.execute("UPDATE inventory SET have ='"+request.json[1]+"', need ='"+ request.json[2] +"', checkweekly ='"+ request.json[3] +"' WHERE id='"+(request.json[0].replace(" ","-")).lower()+"'")
             else:
-                print("INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('"+((request.json[0].replace(" ","-")).lower())+"','"+request.json[0].title()+"','"+request.json[1]+"','"+request.json[2]+"','"+request.json[3]+"','0.00','none','none')")
                 cursor.execute("INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('"+((request.json[0].replace(" ","-")).lower())+"','"+request.json[0].title()+"','"+request.json[1]+"','"+request.json[2]+"','"+request.json[3]+"','0.00','none','none')")
             response = jsonify("Data entry successful!")
         except:
@@ -61,7 +57,7 @@ def getAllInventory():
     inv = db.connect_db()
     cursor = inv.cursor()
     responseList = []
-    print("I don't think there's an item being requested")
+    print("I think all items are being requested")
     serverData = cursor.execute("SELECT * from inventory").fetchall()
     for row in serverData:
         responseList.append([row[0],row[1],row[2],row[3],row[4],row[5]])
