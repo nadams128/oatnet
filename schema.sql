91952a7eebd9d5ab56fdd5bf66ed13b178394da0
Nathan Adams <nathan.zero64@gmail.com>
2024-11-08 20:10:46 -0500

    Disabled testing data in database schema

--- schema.sql
+++ schema.sql
@@ -30,10 +30,10 @@ CREATE TABLE permissions(
     write BOOLEAN NOT NULL
 );
 
--- Initial testing data for inventory items
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('bowls','Bowls','100','15',1,'0.00','none','none');
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('cannedblackbeans','Canned Black Beans','13 cans','8 cans',0,'0.00','none','none');
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('cups','Cups','7','155',1,'0.00','none','none');
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('navybeans','Navy Beans','7lbs','1lbs',0,'0.00','none','none');
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('rice','Rice','8g','None',1,'0.00','none','none');
-INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('spoons','Spoons','1077','0',1,'0.00','none','none');
+-- -- Initial testing data for inventory items
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('bowls','Bowls','100','15',1,'0.00','none','none');
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('cannedblackbeans','Canned Black Beans','13 cans','8 cans',0,'0.00','none','none');
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('cups','Cups','7','155',1,'0.00','none','none');
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('navybeans','Navy Beans','7lbs','1lbs',0,'0.00','none','none');
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('rice','Rice','8g','None',1,'0.00','none','none');
+-- INSERT INTO inventory(id, name, have, need, checkweekly, amountneededweekly, type, location) VALUES ('spoons','Spoons','1077','0',1,'0.00','none','none');
